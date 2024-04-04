ff3c7fe025317ad5a1c43ed7a93df784fe383094
Jakub Lasecki <jakublasecki321@gmail.com>
2024-04-04 18:27:32 +0000

    basic app, healthcheck

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('dupa')
+import express from 'express';
+
+import { PORT } from './constants';
+
+const app = express();
+
+app.get('/healthcheck', (_, res) => {
+	res.status(200).send('OK');
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
