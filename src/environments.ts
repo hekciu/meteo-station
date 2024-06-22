9dc03f52407d3bc677dd30dd8a03323044293e16
Jakub Lasecki <jakublasecki321@gmail.com>
2024-06-22 19:54:00 +0000

    change envs names

--- src/environments.ts
+++ src/environments.ts
@@ -1,6 +1,6 @@
 const ENVIRONMENTS: Record<string, string> = {};
-ENVIRONMENTS.thisApiPassword = process.env.THIS_API_PASSWORD;
-ENVIRONMENTS.thisApiUser = process.env.THIS_API_USER;
+ENVIRONMENTS.THIS_API_PASSWORD = process.env.THIS_API_PAORD;
+ENVIRONMENTS.THIS_API_USER = process.env.THIS_API_USER;
 
 if (!thisApiPassword || !thisApiUser) {
 	throw new Error('Some environment variables are missing, terminating app');
