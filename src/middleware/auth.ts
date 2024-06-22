c3592eee62176f0a7586654a18c511241fa9cb4d
Jakub Lasecki <jakublasecki321@gmail.com>
2024-06-22 23:18:58 +0000

    implemented authorization

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,20 @@
 import ENVIRONMENTS from '../environments';
 import { Request, Response, NextFunction } from 'express';
+import MeteoServerError from '../utils/MeteoServerError';
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
-	console.log(ENVIRONMENTS);	
+	const authString = Buffer.from(`${ENVIRONMENTS.THIS_API_USER}:${ENVIRONMENTS.THIS_API_PASSWORD}`).toString('base64');
+
+	const userAuthHeader = req.get('Authorization');
+
+	if (!userAuthHeader || !userAuthHeader.startsWith('Basic ')) {
+		throw new MeteoServerError('Missing or distorted auth header', 400);
+	}
+
+	if (userAuthHeader.slice(6) !== authString) {
+		throw new MeteoServerError('Authentication failed', 403);
+	}
+
 	next();
 }
 
