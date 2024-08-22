7813b54be76819a18901f9bdaf3d6cc7e064d0d4
Jakub Lasecki <jakublasecki321@gmail.com>
2024-08-22 20:33:12 +0000

    make util function for current timestamp

--- device/test.c
+++ device/test.c
@@ -1,9 +1,7 @@
 #include "httpRequests.h"
+#include "utils.h"
 
 int main() { 
-	char * outputStringPtr = NULL;
-	size_t size = getStringData("localhost:2137/healthcheck", &outputStringPtr); 
-	printf("%s\n", outputStringPtr);
-	postJsonData("localhost:2137/data", "{ \"data\": {} }");
+	printf("Timestamp: %d\n", getCurrentTimestamp());
 	return 0;
 }
--- device/utils.h
+++ device/utils.h
@@ -1,12 +1,19 @@
 #ifndef UTILS
 #define UTILS
 
+#include <time.h>
+#include <stdint.h>
+
 size_t takeSmallerSize(size_t a, size_t b) {
 	if (a < b) {
 		return a;
 	}
 
 	return b;
+}
+
+uint32_t getCurrentTimestamp() {
+	return (uint32_t)time(NULL);
 }
 
 #endif
