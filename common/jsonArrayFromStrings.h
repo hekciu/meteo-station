81655b3eef7811dda9b5d6d50e01d8845816e744
hekciu <jakublasecki321@gmail.com>
2024-10-21 22:17:10 +0200

    implemented function to make json string arrays

--- common/jsonArrayFromStrings.h
+++ common/jsonArrayFromStrings.h
@@ -2,10 +2,11 @@
 #include <string.h>
 
 /*
+    Makes json string array from n separate strings. Need to be called n + 1 times.
     At first this function should be called with pointer set to NULL and first element of array,
     after that it should be called with the same output pointer and next elements as input.
     To close the array, you should call this function with NULL as input. 
-    User is also obligate to free output memory after usage.
+    User is also obligated to free output memory after usage.
 */
 uint64_t jsonArrayFromStrings(char ** output, char * input) {
     uint64_t outputLen;
@@ -15,11 +16,15 @@ uint64_t jsonArrayFromStrings(char ** output, char * input) {
         
         *output = malloc(outputLen);
         snprintf(*output, outputLen, "[\"%s\"", input);
-    } else if (*output != NULL && input == NULL) {
-        uint64_t newContentLen = 
-        outputLen = strlen(*output) + strlen(input) + 4;
+    } else if (*output != NULL && input != NULL) {
+        uint64_t newContentLen = strlen(input) + 4;
+        outputLen = strlen(*output) + newContentLen;
 
-        char * newElementChunk = realloc(*output,      
+        realloc(*output, newContentLen);
+        snprintf(*output + strlen(*output), newContentLen, ",\"%s\"", input);
+    } else if (input == NULL) {
+        realloc(*output, 2);
+        snprintf(*output + strlen(*output), 2, "]");
     }
 
     return outputLen;
