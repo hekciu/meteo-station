d69a45f1b74c9453934d411d7941032783bb88b6
hekciu <jakublasecki321@gmail.com>
2025-04-03 22:50:37 +0200

    a bit of golang server development

--- go-server/main.go
+++ go-server/main.go
@@ -22,13 +22,83 @@ type Pms5003Measurement struct {
 }
 
 
-func getMeasurements(w http.ResponseWriter, r *http.Request) {
-    if r->Method != "GET" {
-        io.WriteString(w, "method not supported\n")
+func _connectToDb() *sql.DB {
+    port := os.Getenv("PORT")
+    thisApiPassword := os.Getenv("THIS_API_PASSWORD")
+    thisApiUsername := os.Getenv("THIS_API_USERNAME")
+    pgPort := os.Getenv("PG_PORT")
+    pgHost := os.Getenv("PG_HOST")
+    pgUser := os.Getenv("PG_USER")
+    pgPassword := os.Getenv("PG_PASSWORD")
+    pgDbname := os.Getenv("PG_DBNAME")
+
+    envs := [...]string{
+        port,
+        thisApiPassword,
+        thisApiUsername,
+        pgPort,
+        pgHost,
+        pgUser,
+        pgPassword,
+        pgDbname,
+    }
+
+    for _, el := range envs {
+        if len(el) == 0 {
+            fmt.Printf("some environment variables are missing\n")
+            return nil
+        }
+    }
+
+	connString := fmt.Sprintf("user=%s dbname=%s port=%s password=%s host=%s", pgUser, pgDbname, pgPort, pgPassword, pgHost)
+
+	db, db_err := sql.Open("postgres", connString)
+
+	if db_err != nil {
+        fmt.Printf("could not create database connection, got: %s\n", db_err)
+        return nil
+	}
+
+    db_err = db.Ping()
+
+    if db_err != nil {
+        fmt.Printf("could not establish database connection, got: %s\n", db_err)
+        return nil
+    }
+
+    return db
+}
+
+
+func postMeasurements(w http.ResponseWriter, r * http.Request) {
+    fmt.Printf("POST /measurements\n")
+    var measurement Pms5003Measurement
+
+    if r.Body == nil {
+        io.WriteString(w, "empty body\n")
         return
     }
 
-    fmt.Printf("got /measurements request\n")
+    decoder := json.NewDecoder(r.Body)
+
+    err := decoder.Decode(&measurement)
+
+    if err != nil {
+        io.WriteString(w, "error parsing body\n")
+        return
+    }
+}
+
+
+func getMeasurements(w http.ResponseWriter, r * http.Request) {
+    fmt.Printf("GET /measurements\n")
+
+    db := _connectToDb()
+
+    if db == nil {
+        io.WriteString(w, "database error!\n")
+        return
+    }
 
     rows, err := db.Query("SELECT * FROM pms5003_measurements")
 
@@ -61,54 +131,24 @@ func getMeasurements(w http.ResponseWriter, r *http.Request) {
     }
 
     io.WriteString(w, string(marshalled))
+    db.Close()
+}
+
+
+func handleMeasurements(w http.ResponseWriter, r * http.Request) {
+    if r.Method == "GET" {
+        getMeasurements(w, r)
+    } else if r.Method == "POST" {
+        postMeasurements(w, r)
+    } else {
+        io.WriteString(w, "method not supported\n")
+    }
 }
 
 
 func main() {
-    port := os.Getenv("PORT")
-    thisApiPassword := os.Getenv("THIS_API_PASSWORD")
-    thisApiUsername := os.Getenv("THIS_API_USERNAME")
-    pgPort := os.Getenv("PG_PORT")
-    pgHost := os.Getenv("PG_HOST")
-    pgUser := os.Getenv("PG_USER")
-    pgPassword := os.Getenv("PG_PASSWORD")
-    pgDbname := os.Getenv("PG_DBNAME")
 
-    envs := [...]string{
-        port,
-        thisApiPassword,
-        thisApiUsername,
-        pgPort,
-        pgHost,
-        pgUser,
-        pgPassword,
-        pgDbname,
-    }
-
-    for _, el := range envs {
-        if len(el) == 0 {
-            fmt.Printf("some environment variables are missing\n")
-            os.Exit(1);
-        }
-    }
-
-	connString := fmt.Sprintf("user=%s dbname=%s port=%s password=%s host=%s", pgUser, pgDbname, pgPort, pgPassword, pgHost)
-
-	db, db_err := sql.Open("postgres", connString)
-
-	if db_err != nil {
-        fmt.Printf("could not create database connection, got: %s\n", db_err)
-        os.Exit(1)
-	}
-
-    db_err = db.Ping()
-
-    if db_err != nil {
-        fmt.Printf("could not establish database connection, got: %s\n", db_err)
-        os.Exit(1)
-    }
-
-    http.HandleFunc("/measurements", getMeasurements);
+    http.HandleFunc("/measurements", handleMeasurements);
 
     server_err := http.ListenAndServe(":3333", nil)
 
